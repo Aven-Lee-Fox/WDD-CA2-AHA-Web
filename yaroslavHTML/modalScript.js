eaafc53f86237b517af81a816b86dbb907d117b1
Arleits <yaroslavrobocraft@gmail.com>
2024-12-07 22:27:33 +0000

    Fixed bug in javascript, added more content to sus1, redid sus2 layout and updated CSS accordingly

--- yaroslavHTML/modalScript.js
+++ yaroslavHTML/modalScript.js
@@ -1,10 +1,11 @@
-function openModal(id) {
+function openModal(element) {
     let text = document.getElementById("myModal"); // Get the element of entire modal box and sends it to text variable
     text.style.display = "block"; // Sets the modal style to block from none which 
 
     let oldText = document.getElementById("modalText").textContent; // Gets the ID of the element of the paragraph that is contained within the modal box
-    let newText = document.getElementById(id.id).textContent; // Gets the ID of the element from where the function has been called
-    newText = newText.replace(oldText, newText);
+    let newText = document.getElementById(element.id).textContent; // Gets the ID of the element from where the function has been called
+    newText.replace(oldText, newText);
+    console.log(newText);
     document.getElementById("modalText").innerHTML = newText;
 }
 
