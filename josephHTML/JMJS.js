4d973ed0c1e41b37529b63b3acf9b9edf5b0a5f3
Joseph <joseph.moiselle05@gmail.com>
2024-12-07 16:48:13 +0000

    made a somewhat working JS function

--- josephHTML/JMJS.js
+++ josephHTML/JMJS.js
@@ -1,34 +1,25 @@
-function autoHide(){
-
-    let doc= document.getElementById("JMShowHide");
-    
-    doc.style.display = "none";
-
-}
-
-
 function showHide(){
 	
 	let doc= document.getElementById("JMShowHide");
     let button= document.getElementById("buttonText");
 	
-		if(doc.style.display == 'none'){
+	if(doc.style.display == 'none'){
 			
-			// if the display is none this will show it
+		// if the display is none this will show it
 			
-			doc.style.display = 'inline';
+		doc.style.display = 'inline';
 
-            button.innerText = "Hide illnesses list";
+        button.innerText = "Hide illnesses list";
 			
-		} else{
+	} else{
 			
-			//if the display is shown this will hide it
+		//if the display is shown this will hide it
 			
-			doc.style.display = 'none';
+		doc.style.display = 'none';
 
-            button.innerText = "Show illnesses list";
+        button.innerText = "Show illnesses list";
 			
-		}
+	}
 	
     
 	
