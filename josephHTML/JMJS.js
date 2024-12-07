57f8a22a36f8fa987d380dbd9a35be9052409768
Joseph <joseph.moiselle05@gmail.com>
2024-12-07 22:04:53 +0000

    got Health1.html's first JS function done

--- josephHTML/JMJS.js
+++ josephHTML/JMJS.js
@@ -1,23 +1,28 @@
+function load(){
+
+    showHide();
+
+}
+
+
 function showHide(){
 	
 	let doc= document.getElementById("JMShowHide");
     let button= document.getElementById("buttonText");
 	
-	if(doc.style.display == 'none'){
+	if(doc.style.display == 'inline'){
 			
-		// if the display is none this will show it
-			
-		doc.style.display = 'inline';
+		
 
-        button.innerText = "Hide illnesses list";
+        doc.style.display = 'none'; //if the display is shown this will hide it
+
+        button.innerText = "Show illnesses list"; // changes the button to say "show" instead of
 			
 	} else{
 			
-		//if the display is shown this will hide it
-			
-		doc.style.display = 'none';
+        doc.style.display = 'inline'; // if the display is none this will show it
 
-        button.innerText = "Show illnesses list";
+        button.innerText = "Hide illnesses list"; // changes the button to say "hide" instead of "show"
 			
 	}
 	
