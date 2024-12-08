5fd0b521e42d2bf0e0d0cfefc4f76593a9901892
Arleits <yaroslavrobocraft@gmail.com>
2024-12-08 15:22:54 +0000

    Modified modal box, modified random target array items and updated sus2 html

--- yaroslavHTML/randomInit.js
+++ yaroslavHTML/randomInit.js
@@ -3,10 +3,9 @@
 
 function randomTarget() {
     const targetList = [ // Text to be used in the generate a random target. Simply adding more, adds more options to be rolled in the random generator
-        "One such target set by the unitied nations, is to ensure sustainable food production via improved systems. This involves the implementation of resilient farming practices that help maintain local ecosystems and strength the capacity of both for climate change and extreme weather conditions",
-        "Insert second target",
-        "Insert third target",
-        "Insert fourth target"
+        "One such target set by the unitied nations, is to ensure sustainable food production via improved systems. This involves the implementation of resilient farming practices that help maintain local ecosystems and strength the capacity of both for climate change and extreme weather conditions. Overall, this would improve the soil quality and over time improve the land allowing future generations to live sustainably with no hunger.",
+        "By 2030, double all agricultural productivity and increase the incomes of local, small producers around the world. The way to achieve this is via secure and equal access to land, knowledge to efficient and sustainable farming practices and providing finanical support through local government bodies.",
+        "Invest into rural infrastructure in less developed regions to help local producers/farmers through international coorporation. Furthur imrpove the farmers sustainable output either through improved technology or using GMOS."
     ];
 
     // This section ensures the same integer is not the same as the previously used one. To make the same target not appear twice or more in a row
