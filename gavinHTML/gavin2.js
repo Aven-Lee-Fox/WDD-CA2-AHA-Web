73a9110cda2ab2afeab81afbeef059acaad336bd
gally05 <gavinlally2005@gmail.com>
2024-12-08 00:46:53 +0000

    js4 updated and html and js2

--- gavinHTML/gavin2.js
+++ gavinHTML/gavin2.js
@@ -4,32 +4,29 @@ function changeImage() {
 }
 
 
-function showHide(){
+function showHide(){ //shows and hides the list of illnesses with a toggle, starts hidden then shows upon click of a button and when shown the button will hide it
 	
 	let doc= document.getElementById("JMShowHide");
     let button= document.getElementById("buttonText");
 	
-	if(doc.style.display == 'none'){
+	if(doc.style.display == 'inline'){
 			
-		// if the display is none this will show it
-			
-		doc.style.display = 'inline';
+		
 
-        button.innerText = "Hide list";
+        doc.style.display = 'none'; //if the display is shown this will hide it
+
+        button.innerText = "Show illnesses list"; // changes the button to say "show" instead of "hide"
 			
 	} else{
 			
-		//if the display is shown this will hide it
-			
-		doc.style.display = 'none';
+        doc.style.display = 'inline'; // if the display is none this will show it
 
-        button.innerText = "Show list";
+        button.innerText = "Hide illnesses list"; // changes the button to say "hide" instead of "show"
 			
 	}
 	
     
 	
 }
-
 
 
--- gavinHTML/gavin4.js
+++ gavinHTML/gavin4.js
@@ -24,3 +24,88 @@ function displayRandomFact() {
 // Variables
 var prevInteger;
 var integer;
+
+
+function validate(){ //the function for the form, checks each field for a valid input then validates
+
+    //fn & sn check that something has been submitted
+    
+    //check for tel > 0
+    
+    //check email for @ symbol
+    
+    //checks to see the date field is not YYYY/MM/DD
+
+
+    
+    let valid = true; //our flag
+    
+    let msge = "incomplete form, please input the following details:<br>";
+    
+    let thx = "";}
+    
+    if(document.getElementById("fn").value == ""){
+    
+        msge+= "First name<br>";
+        
+        valid = false;
+    
+    }
+    
+    if(document.getElementById("sn").value == ""){
+    
+        msge+= "Surname<br>";
+        valid = false;
+        
+    }
+    
+    if(document.getElementById("tel").value == 0){
+    
+        msge+= "Telephone number<br>";
+        valid = false;
+        
+    }
+    
+    if(document.getElementById("email").value == ""){
+    
+        msge+= "Email<br>";
+        valid = false;
+    
+    } else if(document.getElementById("email").value != ""){
+    
+    
+        inputE = document.getElementById("email").value;
+        let symbol = inputE.indexOf("@");
+        
+        if(symbol <1){
+            
+            valid = false;
+            msge+= "Valid email<br>";
+            
+        }
+    
+    }
+
+    
+    if(document.getElementById("DOB").value == ""){
+    
+        msge+= "Please insert your date of birth";
+        valid = false;
+        
+    }
+    
+    if(valid){
+    
+        thx+= "Thank you " + document.getElementById("fn").value + " " + document.getElementById("sn").value + " for filling out the form! We will be in touch soon!";
+        
+        valid = true;
+        
+        document.getElementById("thx").innerHTML = thx;
+        
+    } else{
+
+    document.getElementById("details").innerHTML = msge; //return the flag and show the msge
+    
+    return valid;
+    
+    }
